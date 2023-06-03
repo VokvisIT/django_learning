60df4601e1db0a38127d83816594d1a92c17f4a9
Ivan Sivkov <Vanu20011205@yandex.ru>
2023-06-03 21:45:21 +0300

    Admin compleated
    Доделанная по минимуму админка.

--- lr2/questionnaire/admin.py
+++ lr2/questionnaire/admin.py
@@ -2,3 +2,21 @@ from django.contrib import admin
 
 from questionnaire.models import Answer, Choice, Question
 
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question_text', 'question_type')
+    list_display_links = "id", "question_text",
+    ordering = "id",
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question', 'choice_text')
+    list_display_links = 'id', 'question',
+    ordering = "id",
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question', 'choice', 'answer_text')
+    list_display_links = 'id',
+    ordering = "id",
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
+admin.site.register(Answer, AnswerAdmin)
